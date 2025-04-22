c80cf996c0e15b110c712e9a9addc90cc699f204
kHan <157665884+Batyr006@users.noreply.github.com>
2025-04-23 03:49:52 +0600

    Update settings.py

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mrhan0906@gmail.com'
-EMAIL_HOST_PASSWORD = 'nitnsjzzmlfuueux'
+EMAIL_HOST_USER = 'cxxxxxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxx'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
